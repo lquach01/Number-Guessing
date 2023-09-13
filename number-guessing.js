71a1f6f082bc4bcb42f3ca16b33cf971bc6a768e
Sydney Quach <sydneyq14@gmail.com>
2023-09-12 19:50:13 -0700

    got the main part to work

--- number-guessing.js
+++ number-guessing.js
@@ -1,19 +1,23 @@
-const randomNumber = Math.floor(Math.random() * (500 - 1)) + 1;
-document.getElementById("ans").innerHTML = randomNumber;
+var randomNumber;
+function newGame() {
+    randomNumber = Math.floor(Math.random() * (500 - 1)) + 1;
+    document.getElementById("ans").innerHTML = randomNumber;
+}
 
+function compNums() {
+    var x = document.getElementById("input-number");
+    var givenNum = x.value; 
+    var prevGuesses = document.getElementById("previous-guesses").innerHTML;
 
-const givenNum = document.getElementById("input-number");
-
-
-givenNum.addEventListener('submit', compNums(givenNum));
-
-
-function compNums(num) {
-    if (num < randomNumber) {
+    if (givenNum < randomNumber) {
         document.getElementById("result").innerHTML = "needs to be higher";
-    } else if (num > randomNumber) {
+        document.getElementById("previous-guesses").innerHTML = prevGuesses + " " + givenNum;
+    } else if (givenNum > randomNumber) {
         document.getElementById("result").innerHTML = "needs to be lower";
+        document.getElementById("previous-guesses").innerHTML = prevGuesses + " " + givenNum;
     } else {
         document.getElementById("result").innerHTML = "slay yay";
     }
+
 }
+
